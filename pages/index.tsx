15ab80c8164e15a8359dfef535e57a995f7fa381
Wcraft-dev <70655025+Wcraft-dev@users.noreply.github.com>
2021-11-26 22:46:15 -0500

    ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,27 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import Text from "../components/Text";
 import { NextPage } from "next";
 
+type Ref = {
+  current: THREE.Mesh | undefined;
+};
+type birdProps = {
+  position: THREE.Vector3;
+  rotation: THREE.Euler | undefined;
+  speed: number;
+  factor: number;
+  url: string;
+};
+
 function Jumbo() {
-  const ref = useRef();
-  useFrame(
-    ({ clock }) =>
-      (ref.current.rotation.x =
+  const ref: Ref = useRef();
+  useFrame(({ clock }) => {
+    if (ref.current) {
+      ref.current.rotation.x =
         ref.current.rotation.y =
         ref.current.rotation.z =
-          Math.sin(clock.getElapsedTime()) * 0.3)
-  );
+          Math.sin(clock.getElapsedTime()) * 0.3;
+    }
+  });
   return (
     <group ref={ref}>
       <Text hAlign="right" position={[-12, 6.5, 0]} children="JHAIR" />
@@ -23,26 +35,31 @@ function Jumbo() {
 }
 
 // This component was auto-generated from GLTF by: https://github.com/react-spring/gltfjsx
-function Bird({ speed, factor, url, ...props }) {
-  const { nodes, materials, animations } = useLoader(GLTFLoader, url);
-  const group = useRef();
-  const mesh = useRef();
+function Bird({ speed, factor, url, ...props }: birdProps) {
+  const model = useLoader(GLTFLoader, url);
+  const { nodes, materials, animations }: any = model;
+  const group: Ref = useRef();
+  const mesh: Ref = useRef();
   const [start] = useState(() => Math.random() * 5000);
-  const [mixer] = useState(() => new THREE.AnimationMixer());
+  const [mixer] = useState(() => new THREE.AnimationMixer(new THREE.Group()));
   useEffect(
     () => void mixer.clipAction(animations[0], group.current).play(),
     []
   );
   useFrame((state, delta) => {
-    mesh.current.position.y = Math.sin(start + state.clock.elapsedTime) * 5;
-    mesh.current.rotation.x =
-      Math.PI / 2 + (Math.sin(start + state.clock.elapsedTime) * Math.PI) / 10;
-    mesh.current.rotation.y =
-      (Math.sin(start + state.clock.elapsedTime) * Math.PI) / 2;
-    group.current.rotation.y +=
-      Math.sin((delta * factor) / 2) * Math.cos((delta * factor) / 2) * 1.5;
-    mixer.update(delta * speed);
+    if (group.current && mesh.current) {
+      mesh.current.position.y = Math.sin(start + state.clock.elapsedTime) * 5;
+      mesh.current.rotation.x =
+        Math.PI / 2 +
+        (Math.sin(start + state.clock.elapsedTime) * Math.PI) / 10;
+      mesh.current.rotation.y =
+        (Math.sin(start + state.clock.elapsedTime) * Math.PI) / 2;
+      group.current.rotation.y +=
+        Math.sin((delta * factor) / 2) * Math.cos((delta * factor) / 2) * 1.5;
+      mixer.update(delta * speed);
+    }
   });
+
   return (
     <group ref={group} dispose={null}>
       <scene name="Scene" {...props}>
@@ -61,31 +78,37 @@ function Bird({ speed, factor, url, ...props }) {
   );
 }
 
-function Birds() {
-  return new Array(10).fill().map((_, i) => {
-    const x = (15 + Math.random() * 30) * (Math.round(Math.random()) ? -1 : 1);
-    const y = -10 + Math.random() * 20;
-    const z = -5 + Math.random() * 10;
-    const bird = ["Stork", "Parrot", "Flamingo"][Math.round(Math.random() * 2)];
-    let speed = bird === "Stork" ? 0.25 : bird === "Flamingo" ? 0.5 : 5;
-    let factor =
-      bird === "Stork"
-        ? 0.5 + Math.random()
-        : bird === "Flamingo"
-        ? 0.25 + Math.random()
-        : 1 + Math.random() - 0.5;
-    return (
-      <Bird
-        key={i}
-        position={[x, y, z]}
-        rotation={[0, x > 0 ? Math.PI : 0, 0]}
-        speed={speed}
-        factor={factor}
-        url={`/${bird}.glb`}
-      />
-    );
-  });
-}
+const Birds = () => {
+  const arrayBirds = Array<number>(10)
+    .fill(0)
+    .map((_, i) => {
+      const x =
+        (15 + Math.random() * 30) * (Math.round(Math.random()) ? -1 : 1);
+      const y = -10 + Math.random() * 20;
+      const z = -5 + Math.random() * 10;
+      const bird = ["Stork", "Parrot", "Flamingo"][
+        Math.round(Math.random() * 2)
+      ];
+      let speed = bird === "Stork" ? 0.25 : bird === "Flamingo" ? 0.5 : 5;
+      let factor =
+        bird === "Stork"
+          ? 0.5 + Math.random()
+          : bird === "Flamingo"
+          ? 0.25 + Math.random()
+          : 1 + Math.random() - 0.5;
+      return (
+        <Bird
+          key={i}
+          position={new THREE.Vector3(x, y, z)}
+          rotation={new THREE.Euler(0, x > 0 ? Math.PI : 0, 0)}
+          speed={speed}
+          factor={factor}
+          url={`/${bird}.glb`}
+        />
+      );
+    });
+  return <>{arrayBirds}</>;
+};
 
 const App: NextPage = () => {
   return (
